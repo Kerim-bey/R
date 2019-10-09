3f36eb66f2179d359bd0e34cd01b1a46595e7a86
Kerim <kerim_robo@mail.ru>
2019-10-09 21:47:36 +0300

    Add files via upload

---
+++ knn.R
@@ -0,0 +1,44 @@
+
+euclideanDistance <- function(u, v)
+{
+  sqrt(sum((u - v)^2))
+}
+
+sortObjectsByDist <- function(xl, z, metricFunction =
+                                euclideanDistance)
+{
+  l <- dim(xl)[1]
+  n <- dim(xl)[2] - 1
+  ## Создаём матрицу расстояний
+  distances <- matrix(NA, l, 2)
+  for (i in 1:l)
+  {
+    distances[i, ] <- c(i, metricFunction(xl[i, 1:n], z))
+  }
+  ## Сортируем
+  orderedXl <- xl[order(distances[, 2]), ]
+  return (orderedXl);
+}
+
+kNN <- function(xl, z, k)
+{
+  ## Сортируем выборку согласно классифицируемого
+  orderedXl <- sortObjectsByDist(xl, z)
+  n <- dim(orderedXl)[2] - 1
+  ## Получаем классы первых k соседей
+  classes <- orderedXl[1:k, n + 1]
+  counts <- table(classes)
+  class <- names(which.max(counts))
+  return (class)
+}
+## Рисуем выборку
+colors <- c("setosa" = "red", "versicolor" = "green3",
+            "virginica" = "blue")
+plot(iris[, 3:4], pch = 21, bg = colors[iris$Species], col
+     = colors[iris$Species], asp = 1)
+## Классификация одного заданного объекта
+z <- c(2.7, 1)
+xl <- iris[, 3:5]
+class <- kNN(xl, z, k=6)
+points(z[1], z[2], pch = 22, bg = colors[class], asp = 1)
+
